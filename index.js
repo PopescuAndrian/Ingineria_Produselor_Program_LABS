6ac43c8dd5f8e8fa128bc6f6bb3cc042fd46479a
Andrian Popescu <andrian@linde>
2025-02-10 14:36:16 +0200

    New work done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello World')
+fetchHomePage();
+
+async function fetchHomePage() {
+
+
+  try {
+    const response = await fetch("https://www.reddit.com/r/Home.json");
+    if (!response.ok) {
+      throw new Error("Could not fetch");
+    }
+
+    const json = await response.json();
+    console.log(json);
+
+  } catch (error) {
+    console.error(error.message);
+  }
+}
